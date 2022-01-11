910e64d56a53c728c31c415a9e5277a75c632cb1
Renato Santos <santos.renato@hotmail.com>
2022-01-11 15:16:44 -0300

    arrumando menu

---
+++ script.js
@@ -0,0 +1,24 @@
+var swiper = new Swiper(".cocaSwiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+        rotate: 0,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+    },
+    loop: true
+});
+
+function toggleMenu(){
+    const menuToggle = document.querySelector('.toggle');
+    const navigation = document.querySelector('.navigation')
+    menuToggle.classList.toggle('active')
+    navigation.classList.toggle('active')
+}
